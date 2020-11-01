47d72a8d2e6822252cc382d3a2263a88e0e1ed5c
naveen <naveenreddy1377@gmail.com>
2020-11-01 12:42:40 +0530

    first commit

---
+++ redirect/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    articlename=models.CharField(max_length=50)
+    counter=models.IntegerField()
+
+    
+    
